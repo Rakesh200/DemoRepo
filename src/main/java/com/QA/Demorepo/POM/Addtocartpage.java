55b0ed8e80c66cb3c253c3143678afa4e0be2ccd
rakesh <rakesh.ganapathi96@gmail.com>
2018-03-02 16:07:53 +0530

    changes made in addtocart page

--- src/main/java/com/QA/Demorepo/POM/Addtocartpage.java
+++ src/main/java/com/QA/Demorepo/POM/Addtocartpage.java
@@ -1,5 +1,17 @@
 package com.QA.Demorepo.POM;
 
 public class Addtocartpage {
+	
+	public void addtocartpage(){
+		System.out.println("Addtocartpage");
+	}
+	
+	public void addtocartpage1(){
+		System.out.println("Addtocartpage");
+	}
+	
+	public void addtocartpage2(){
+		System.out.println("Addtocartpage");
+	}
 
 }
